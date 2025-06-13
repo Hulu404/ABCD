a8be69a0c8fb5540290567f9c9be7edcd2cbf1d7
Ali Bnayat <hulumulu680@mail.ru>
2025-06-13 10:48:40 +0300

    added registration

--- app/handlers.py
+++ app/handlers.py
@@ -2,8 +2,13 @@ from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, CallbackQuery
 import app.Keyboards as kb
-
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
 router = Router()
+
+class Reg(StatesGroup):
+    name = State()
+    number = State()
 
 
 @router.message(CommandStart())
@@ -46,4 +51,24 @@ async def basket(callback: CallbackQuery):
 async def return_(callback: CallbackQuery):
     await callback.answer("Main")
     await callback.message.edit_text("You on start", reply_markup=kb.main)
+
+
+@router.message(Command("reg"))
+async def reg_one(message: Message, state: FSMContext):
+    await state.set_state(Reg.name)
+    await message.answer("What's your name?")
+
+@router.message(Reg.name)
+async def reg_two(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(Reg.number)
+    await message.answer("Write your phone number:")
+
+@router.message(Reg.number)
+async def two_three(message: Message, state: FSMContext):
+    await state.update_data(number=message.text)
+    data = await state.get_data()
+    await message.answer(f"Thank u! You're registered. \nName: {data["name"]}\nNumber: {data["number"]}")
+    await state.clear()
+
 
